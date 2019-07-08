2adcfce6e17b070b497b70f762e4695e035bab62
Brent Sowers <brentsowers1@gmail.com>
2019-07-07 20:01:55 -0400

    Added polygons on the map

--- src/containers/Map.js
+++ src/containers/Map.js
@@ -2,7 +2,8 @@ import React from "react";
 import {
   withScriptjs,
   withGoogleMap,
-  GoogleMap
+  GoogleMap,
+  Polygon
 } from "react-google-maps";
 import { compose, withProps } from "recompose";
 import { connect } from "react-redux";
@@ -21,21 +22,47 @@ const Map = compose(
   withGoogleMap
 )(props => (
   <GoogleMap
-    zoom={4}
+    zoom={5}
     center={props.center}
   >
-
+    {props.polygons.map(polygon => 
+      <Polygon
+        key={polygon.id}
+        path={polygon.coordinates}
+        options={{
+          strokeColor: "#000000",
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: "#111111",
+          fillOpacity: 0.1
+        }}
+      />
+    )}  
   </GoogleMap>
 ));
 
 const mapStateToProps = state => {
   const selectedPlace = getSelectedPlace(state);
-  console.log("in Map.js, selectedPlace = ", selectedPlace);
+  const polygons = state.places.placeTypes && state.places.placeTypes[state.places.activePlaceType] ?
+    state.places.placeTypes[state.places.activePlaceType].map(place => {
+      return {
+        id: place.id,
+        coordinates: place.coordinates[0].map(polygonCoords => {
+          return {
+            lat: polygonCoords[1],
+            lng: polygonCoords[0]};
+          }
+        )
+      };
+    })
+    : [];
+  console.log("polygons - ", polygons);
   return {
     center: selectedPlace ? 
       getLatLng(selectedPlace.center) : 
-      getLatLng([-77, 40])
-  }
+      getLatLng([-98.5795, 39.8283]),
+    polygons: polygons
+  };
 }
 
 export default connect(mapStateToProps)(Map);
