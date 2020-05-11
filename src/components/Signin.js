a20476aad25042e59429da7fdcef09e2027248d5
Brent Sowers <brentsowers1@gmail.com>
2020-05-11 19:56:06 -0400

    Refactored signin to be a functional component and added redirection after successful login

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,76 +1,70 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import CognitoAuth from '../classes/CognitoAuth';
+import { useHistory } from 'react-router-dom';
 
-class Signin extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      password: '',
-      cognitoError: null
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit(event) {
+const Signin = (props) => {
+  const history = useHistory();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [cognitoError, setCognitoError] = useState(null); 
+  
+  const handleSubmit = (event) => {
     event.preventDefault();
     CognitoAuth.signin(
-      this.state.username, 
-      this.state.password,
-      this.handleLoginSuccessCallback.bind(this),
-      this.handleLoginFailureCallback.bind(this));
+      username, 
+      password,
+      handleLoginSuccessCallback,
+      handleLoginFailureCallback);
   }
 
-  handleLoginSuccessCallback() {
-    this.setState({cognitoError: null});
-    
+  const handleLoginSuccessCallback = () => {
+    setCognitoError(null);
+    history.push('/');
   }
 
-  handleLoginFailureCallback(err) {
-    this.setState({cognitoError: err.message});
+  const handleLoginFailureCallback = (err) => {
+    setCognitoError(err.message);
   }
 
-  render() {
-    return (
-      <Container>
-        <h1>Sign In To Coordinate Commons</h1>
-        <form onSubmit={this.handleSubmit}>
-          <Row>
-            <Col md={2}>
-              <label htmlFor='username'>Username:</label>
-            </Col>
-            <Col md={3}>
-              <input type='text' name='username' onChange={(e) => this.setState({username: e.target.value})} required />
-            </Col>
-          </Row>
-          <Row>
-            <Col md={2}>
-              <label htmlFor='password'>Password:</label>
-            </Col>
-            <Col md={3}>
-              <input type='password' name='password' onChange={(e) => this.setState({password: e.target.value})} required />
-            </Col>
-          </Row>
-          <Row>
-            <Col md={5}>
-              <input type='submit' value='Log in' />
-            </Col>
-          </Row>
-        </form>
-        {this.state.cognitoError ? 
-          <div>
-            <div>Error attempting to log in:</div>
-            <div>{this.state.cognitoError}</div>
-          </div> : ''
-        }
+  return (
+    <Container>
+      <h1>Sign In To Coordinate Commons</h1>
+      <form onSubmit={handleSubmit}>
+        <Row>
+          <Col md={2}>
+            <label htmlFor='username'>Username:</label>
+          </Col>
+          <Col md={3}>
+            <input type='text' name='username' onChange={(e) => setUsername(e.target.value)} required />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={2}>
+            <label htmlFor='password'>Password:</label>
+          </Col>
+          <Col md={3}>
+            <input type='password' name='password' onChange={(e) => setPassword(e.target.value)} required />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={5}>
+            <input type='submit' value='Log in' />
+          </Col>
+        </Row>
+      </form>
+      {cognitoError ? 
         <div>
-          Don't have an account? <Link to='/signup'>Sign Up Here!</Link>
-        </div>
-      </Container>
-    );
-  }
-};
+          <div>Error attempting to log in:</div>
+          <div>{cognitoError}</div>
+        </div> : ''
+      }
+      <div>
+        Don't have an account? <Link to='/signup'>Sign Up Here!</Link>
+      </div>
+    </Container>
+  );
+}
 
 export default Signin;
