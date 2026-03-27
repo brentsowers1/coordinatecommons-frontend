e385eb5b1e836a1ea65961f5867227aeb93d8183
Brent Sowers <brentsowers1@gmail.com>
2026-03-27 17:03:40 -0400

    Added code coverage report command

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/app/',
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      include: ['src/**/*.{js,jsx}'],
+      exclude: ['src/test/**', 'src/main.jsx'],
+    }
+  }
 });
